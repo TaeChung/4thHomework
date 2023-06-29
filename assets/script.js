3f0e0b78d88a4a338d946de3c0f39aab9c56a8f9
TaeChung <taewc95@gmail.com>
2023-06-28 21:09:17 -0400

    first commit

---
+++ assets/script.js
@@ -0,0 +1,98 @@
+var answerInput = document.querySelector("answer")
+var addButton = document.querySelector("add")
+var subtraction = document.querySelector("subtract")
+var counter = document.querySelector("counter")
+var userScore = document.querySelector("#Score")
+var timer = document.querySelector("timer")
+var startButton = document.querySelector(".Start-Button")
+var Timer; 
+var Score= 0
+var trackIndex = 0
+userScore.textContent=Score
+var questionsList = [
+	{
+		question: "How old was Augustus when he formed and became part of the Second Triumvirate?",
+		choices: ["31", "20", "38", "27",],
+		rightanswer: "20"
+	},
+	{
+		question: "How many denominations of Protestanism are there?",
+		choices: ["30", "55", "800", "1000+",],
+		rightanswer: "1000+"
+	},
+	{
+		question: "During which Monarch's reign was Hagia Sophia constructed?",
+		choices: ["Augustus", "Aurelian", "Heraclius", "Justinian I",],
+		rightanswer: "Justinian I"
+	},
+	{
+		question: "Which Sultan conquered Istanbul/Constantinople?",
+		choices: ["Mehmet II", "Suleiman", "Selim I", "Selim II",],
+		rightanswer: "Mehmet II"
+	},
+	{
+		question: "Which nation has the oldest standing structure in the world?",
+		choices: ["Turkey-Gobekli Tepe", "Egypt-Pyramids of Giza", "Malta-The Megalithic Temples of Malta", "Greece-Parthenon",],
+		rightanswer: "Turkey-Gobekli Tepe. It is around 9500-11000 years old."
+	},
+]
+
+startButton.addEventListener("click", startQuiz)
+
+function startQuiz() {
+	showQuestions()
+}
+
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+
+
+
+	function showResults(questions, quizContainer, resultsContainer) {
+
+	}
+
+
+	showQuestions(questions, quizContainer);
+
+
+	submitButton.onclick = function () {
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
+
+function showQuestions() {
+	var h3 = document.querySelector("#Question-Storage")
+	h3.textContent = questionsList[trackIndex].question
+	document.querySelector(".buttons").textContent=""
+	questionsList[trackIndex].choices.forEach(function (choices) {
+		var button = document.createElement("button")
+		button.textContent = choices
+		button.addEventListener("click", function(event){
+			AITA(event)
+		}) 
+		document.querySelector(".buttons").append(button)
+	})
+}
+
+function AITA(event) {
+	console.log("button-clicked")
+	trackIndex++
+	if (event.target.textContent == questionsList[trackIndex].rightanswer){
+		Score++
+		userScore.textContent=Score
+	}
+	showQuestions()
+	
+	
+
+	// check comment
+}
+
+function checkScore(){
+	
+}
+
+Timer = setInterval(function(){
+	console.log(Score)
+},1000) 
